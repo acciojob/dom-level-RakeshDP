f14eb4698ee2f1b49c137146dfbbc0d0cd35e562
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-27 17:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.getElementById("level");
+  const level = getDOMLevel(element);
+  alert(`The level of the element is: ${level}`);
+});
+
+function getDOMLevel(element) {
+  let level = 0;
+  let currentElement = element;
+
+  while (currentElement.parentNode) {
+    currentElement = currentElement.parentNode;
+    level++;
+  }
+
+  return level;
+}
